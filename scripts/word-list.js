27191ed3204492bdbeebd18aa8758dcd0abd3961
ahmedkhilla <92396615+ahmedkhilla@users.noreply.github.com>
2023-09-02 13:37:19 +0200

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,26 @@
+const wordList = [
+  {
+    word: "guitar",
+    hint: "A musical instument with strings.",
+  },
+  {
+    word: "oxygen",
+    hint: "A colorless, odorless gas essintial for life.",
+  },
+  {
+    word: "sunset",
+    hint: "The daily disapperance of the sun below the horizon",
+  },
+  {
+    word: "bicycle",
+    hint: "A human-powered vehicle with two wheels",
+  },
+  {
+    word: "coffee",
+    hint: "A popular caffeinated beverage made from roased coffee beans",
+  },
+  {
+    word: "dance",
+    hint: "A rythmic movement of the body often performed to music",
+  },
+];
